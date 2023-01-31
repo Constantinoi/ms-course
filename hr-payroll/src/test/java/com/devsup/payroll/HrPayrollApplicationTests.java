9d933783efab2c4a877dcb19afbacbc3d6678d25
Constantino Barreto <pescaalma@gmail.com>
2023-01-30 21:00:49 -0300

    hr-payroll

---
+++ hr-payroll/src/test/java/com/devsup/payroll/HrPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsup.payroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
