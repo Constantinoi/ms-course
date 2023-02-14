2ee0906a3a92648e4c30766b67e0bf898561fbcc
Constantino Barreto <pescaalma@gmail.com>
2023-02-13 22:20:35 -0300

    interligando dois projetos

--- hr-payroll/src/main/java/com/devsup/payroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/devsup/payroll/services/PaymentService.java
@@ -1,15 +1,33 @@
 package com.devsup.payroll.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.devsup.payroll.entities.Payment;
+import com.devsup.payroll.entities.Worker;
 
 @Service
 public class PaymentService {
 
+	@Value("${hr-worker.host}")
+	private String workerHost;
+
+	@Autowired
+	private RestTemplate restTemplate;
+
 	public Payment getPayment(Long workedId, int days) {
 
-		return new Payment("Bob", 200.0, days);
+		Map<String, String> uriVariable = new HashMap<>();
+
+		uriVariable.put("id", workedId.toString());
+
+		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariable);
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 
 	}
 
